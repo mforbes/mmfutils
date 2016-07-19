6a573e34fed4a81ebfffe577a9c84540ef3954a3
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2016-07-19 15:38:17 -0700

    WIP: Preliminary work on some plotting tools.

--- mmfutils/tests/test_plot.py
+++ mmfutils/tests/test_plot.py
@@ -1,4 +1,8 @@
+import os
+import tempfile
+
 import numpy as np
+from matplotlib import pyplot as plt
 
 from mmfutils import plot as mmfplt
 
@@ -14,3 +18,22 @@ class TestMidpointNormalize(object):
         assert np.allclose(
             A.mask,
             mmfplt.MidpointNormalize()(A).mask)
+
+
+class TestRasterize(object):
+    def test_contourf(self):
+        with tempfile.NamedTemporaryFile(suffix='.pdf') as f:
+            x, y = np.meshgrid(*(np.linspace(-1, 1, 500),)*2)
+            z = np.sin(20*x**2)*np.cos(30*y)
+            plt.contourf(x, y, z, 30)
+
+            plt.savefig(f.name)
+            size_unrasterized = os.stat(f.name).st_size
+
+            plt.clf()
+            mmfplt.contourf(x, y, z, 30, rasterized=True)
+
+            plt.savefig(f.name)
+            size_rasterized = os.stat(f.name).st_size
+
+        assert size_rasterized < size_unrasterized/30
