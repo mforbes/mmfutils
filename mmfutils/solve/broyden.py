BUG,API,ENH: Resolves issue #20: DyadicSum and scipy.optimize.nonlin.Jacobian

mmfutils/solve/broyden.py
@@ -212,6 +212,14 @@ class DyadicSum(object):
 
         self.orthogonalize()
 
+    @property
+    def dtype(self):
+        return np.asarray(self._at).dtype
+
+    @property
+    def shape(self):
+        return (self._at.shape[0], self._b.shape[1])
+
     def copy(self):
         """Return a (deep) copy of self."""
         return copy.deepcopy(self)
@@ -676,11 +684,11 @@ class JacobianBFGS(sp.optimize.nonlin.Jacobian):
 
     @property
     def dtype(self):
-        return self._dx[0].dtype
+        return np.asarray(self._last_f).dtype
 
     @property
     def shape(self):
-        return (len(self._dx[0]),)*2
+        return (len(np.asarray(self._last_f)),)*2
 
     @property
     def _eye(self):
@@ -734,7 +742,7 @@ class JacobianBFGS(sp.optimize.nonlin.Jacobian):
             rho = 1./(y.dot(s))
             V = I - rho*y[:, None]*s[None, :]
             H = V.T.dot(H).dot(V) + rho*s[:, None]*s[None, :]
-        return H
+        return np.asarray(H)
 
     def dense_J(self):
         """Return a dense array with the Jacobian J."""
@@ -746,6 +754,9 @@ class JacobianBFGS(sp.optimize.nonlin.Jacobian):
         if self._last_x is not None:
             dx = x - self._last_x
             df = f - self._last_f
+            if np.allclose(0, df.dot(dx), atol=_EPS**2):
+                raise np.linalg.LinAlgError(
+                    "Current step makes Jacobian singular.")
             self._dx.append(dx)
             self._df.append(df)
             if len(self._dx) > self.n_max:
@@ -763,13 +774,10 @@ class Jacobian(DyadicSum):
         self.method = method
         
     def solve(self, v):
-        return self.dot(v)
+        return self.inv().dot(v)
 
     def rsolve(self, v):
-        return self.__rmul__(v)
-
-    def todense(self):
-        return self.asarray()
+        return self.inv().__rmul__(v)
 
     def update(self, x, f):
         if self.last_x is not None:
@@ -778,6 +786,12 @@ class Jacobian(DyadicSum):
             self.update_broyden(dx=dx, df=df, method=self.method)
         self.last_x = np.copy(x)
         self.last_f = np.copy(f)
+
+    def matvec(self, v):
+        return self.__mul__(v)
+
+    def rmatvec(self, v):
+        return self.__rmul__(v)
 
 
 class L_BFGS(object):

mmfutils/solve/tests/test_broyden.py
@@ -1,6 +1,8 @@
 import pytest
 
 import numpy as np
+import scipy.optimize.nonlin
+import scipy as sp
 
 from mmfutils.solve import broyden
 
@@ -24,6 +26,20 @@ def dyadic_sum(alpha):
     B.add_dyad(a.T, b)
     B_ = alpha*np.eye(4) + a.T.dot(b)
     yield (B, B_)
+
+
+@pytest.fixture
+def jacobian(alpha):
+    np.random.seed(1)
+    a = np.random.random((3, 4)) - 0.5
+    b = np.random.random((3, 4)) - 0.5
+    B = broyden.Jacobian(alpha=alpha, n_max=np.inf)
+    B.add_dyad(a.T, b)
+    B_ = alpha*np.eye(4) + a.T.dot(b)
+    assert np.allclose(B_, B.todense())
+    B_ = sp.optimize.nonlin.asjacobian(B_)
+    x = np.random.random(4) - 0.5
+    yield (B, B_, x)
     
     
 class TestDyadicSum(object):
@@ -37,7 +53,7 @@ class TestDyadicSum(object):
         A = np.random.random(B_.shape) - 0.5
         
         assert np.allclose(B_, B.todense())
-        #assert np.allclose(A.dot(B_), A.dot(B))
+        # assert np.allclose(A.dot(B_), A.dot(B))
         assert np.allclose(A.dot(B_), (np.asmatrix(A)*B))
         assert np.allclose(B_.dot(A), B.dot(A))
 
@@ -93,9 +109,13 @@ class TestJacobianBFGS(object):
         N = 4
         dxs = np.random.random((k, N)) - 0.5
         dfs = np.random.random((k, N)) - 0.5
+        x = np.random.random(N) - 0.5
+        f = np.random.random(N) - 0.5
         v = np.random.random(N) - 0.5
+        J.update(x, f)
         for dx, df in zip(dxs, dfs):
-            J.update(dx, df)
+            x, f = x+dx, f+df
+            J.update(x, f)
             H = J.dense_H()
             assert np.allclose(H.dot(df), dx)
             assert np.allclose(J.solve(df), dx)
@@ -107,5 +127,11 @@ class TestJacobianBFGS(object):
     
 class TestDyadicSumJacobian(object):
     """Test the `scipy.optimize.nonlin.Jacobian` interface."""
-    def test_1(self, dyadic_sum):
-        B, B_ = dyadic_sum
+    def test_1(self, jacobian):
+        B, B_, x = jacobian
+        assert B.dtype == B_.dtype
+        assert B.shape == B_.shape
+        assert np.allclose(B.solve(v=x), B_.solve(v=x))
+        assert np.allclose(B.rsolve(v=x), B_.rsolve(v=x))
+        assert np.allclose(B.matvec(v=x), B_.matvec(v=x))
+        assert np.allclose(B.rmatvec(v=x), B_.rmatvec(v=x))